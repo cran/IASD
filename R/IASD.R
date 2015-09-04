48ae0b44eb8586d0c82a19082bac36d88fdf8474
Satoshi Takahashi <takahasi@lisboa.ics.nara-wu.ac.jp>
2015-09-04 21:49:37 +0000

    version 1.1

--- R/IASD.R
+++ R/IASD.R
@@ -1,9 +1,14 @@
 IASD <-
-function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE, 
-		plotToScreen = FALSE, filePrefix = "Graph", xlimMin = NA, xlimMax = NA, 
-		ylimMin = 0, ylimMax = NA, dHist = NA, dFunc = NA, meanStartSymmetric = NA, 
-		sdStartSymmetric = NA, meanStartAsymmetric = NA, sdStartAsymmetric = NA, 
-		positiveRatioStartAsymmetric = NA){  
+function(df, dfCols = NA, fixSignApproximation = FALSE, 
+		plotGraph = TRUE, plotToScreen = FALSE, filePrefix = NA, 
+		xlimMin = NA, xlimMax = NA, ylimMin = 0, ylimMax = NA, 
+		dHist = NA, dFunc = NA, meanStartSymmetric = NA, 
+		sdStartSymmetric = NA, meanStartAsymmetric = NA, 
+		sdStartAsymmetric = NA, positiveRatioStartAsymmetric = NA, 
+		plotSelect = rep(TRUE, 4), showLegend = TRUE, 
+		modelName = c("FA", "DA", "AS", "Skewed AS"), xlab = NA, 
+		ylab = NA, main = NA, freqAxis = FALSE, lineColor = "black", 
+		nsmall = 2, fileType = "TEXT", ...){  
 	# df: data frame
 	# range: range of the distribution graph, 
 	# dHist: division width of histgram
@@ -19,31 +24,36 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 		p*n/(n-p-1)
 	}
 
-	PlotHist <- function(ylimMin, ylimMax){
+	PlotHist <- function(ylimMin, ylimMax, freq = F){
 		if (is.na(ylimMin) || is.na(ylimMax))
-			hist(ias, freq=F, breaks=breaks, xlab=colname, 
-				main=sprintf("Histogram of %s", colname))
+			hist(ias, freq = freq, breaks = breaks, xlab = xlab[i], ylab = ylab[i], main = main[i], ...)
 		else
-			hist(ias, freq=F, breaks=breaks, ylim = c(ylimMin, ylimMax), xlab=colname, 
-				main=sprintf("Histogram of %s", colname))
+			hist(ias, freq = freq, breaks = breaks, ylim = c(ylimMin, ylimMax), xlab = xlab[i], ylab = ylab[i], main = main[i], ...)
+	}
+	
+	AddPrefix <- function(name){
+		if (is.na(filePrefix))
+			return(name)
+		else
+			return(paste0(filePrefix, "-", name))
 	}
 		
-	if (is.na(cols)){
+	if (is.na(dfCols)){
 		ncdf <- ncol(df)
-		if (ncdf >1)
-			cols = c(2:ncdf)
+		if (ncdf > 1)
+			dfCols = c(2:ncdf)
 		else
-			cols <- 1
+			dfCols <- 1
 	}
-	nc <- length(cols)
-	sd1 <- rep(0, nc)
-	sd2 <- rep(0, nc)
-	sd3 <- rep(0, nc)
-	sd4 <- rep(0, nc)
-	mean2 <- rep(0, nc)
-	mean3 <- rep(0,nc)
-	mean4 <- rep(0, nc)
-	positiveRatio4 <- rep(0, nc)
+	nc <- length(dfCols)
+	sd1 <- 0
+	sd2 <- 0
+	sd3 <- 0
+	sd4 <- 0
+	mean2 <- 0
+	mean3 <- 0
+	mean4 <- 0
+	positiveRatio4 <- 0
 	UniSymAIC <- rep(0, nc)
 	UniSymAICC <- rep(0, nc)
 	UniAsymAIC <- rep(0, nc)
@@ -56,6 +66,11 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 	UnimodalAsymmetric <- as.list(NA, nc)
 	BimodalSymmetric <- as.list(NA, nc)
 	BimodalAsymmetric <- as.list(NA, nc)
+	
+	AIC.df <- data.frame(dummy = rep(0,4))
+	rownames(AIC.df) <- modelName
+	AICc.df <- data.frame(dummy = rep(0,4))
+	rownames(AICc.df) <- modelName
 	
 	RepVar <- function(x){
 		if (length(x) >= nc)
@@ -76,12 +91,18 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 		meanStartAsymmetric <- RepVar(meanStartAsymmetric)
 		sdStartAsymmetric <- RepVar(sdStartAsymmetric)
 		positiveRatioStartAsymmetric <- RepVar(positiveRatioStartAsymmetric)
+		xlab <- RepVar(xlab)
+		ylab <- RepVar(ylab)
+		main <- RepVar(main)
 	}
+	if (length(lineColor) == 1)
+		lineColor <- rep(lineColor, 4)
 	i <- 0
-	for(col in cols){
+	for(clm in dfCols){
 		i <- i+1
-		colname <- colnames(df)[col]
-		ias <- df[[col]]
+		lty <- 0
+		colname <- colnames(df)[clm]
+		ias <- df[[clm]]
 		n <- length(ias)
 		absMax = max(abs(ias))
 		if (absMax > 6)
@@ -104,33 +125,31 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 		df$PUniSym <- dnorm(ias, sd = sd)
 		UniSymAIC[i] <- -2*sum(log(df$PUniSym)) + 2*1
 		UniSymAICC[i] <- -2*sum(log(df$PUniSym)) + 2*AICC(1,n)
-		sd1[i] <- sd
-		switch(i,
-			f <- function(x){dnorm(x, sd = sd1[1])},
-			f <- function(x){dnorm(x, sd = sd1[2])},
-			f <- function(x){dnorm(x, sd = sd1[3])},
-			f <- function(x){dnorm(x, sd = sd1[4])},
-			f <- function(x){dnorm(x, sd = sd1[5])},
-			f <- function(x){dnorm(x, sd = sd1[6])},
-			f <- function(x){dnorm(x, sd = sd1[7])},
-			f <- function(x){dnorm(x, sd = sd1[8])},
-			f <- function(x){dnorm(x, sd = sd1[9])},
-			f <- function(x){dnorm(x, sd = sd1[10])},
-		)
+
+		f <- eval(parse(text = paste("function(x){dnorm(x, sd =", sd, ")}", 
+			srcfile = NULL)))	
+
 		UnimodalSymmetric[[i]] <- list(AIC = UniSymAIC[i], AICc = UniSymAICC[i], 
 			sd = sd, f = f)
 		names(UnimodalSymmetric)[i] = colname
 		if (plotGraph){
-			fileName = sprintf("%s-%s-Unimodal-Symmetric.pdf", 
-				filePrefix, colname)
 			if (!plotToScreen)
-				pdf(file = fileName)
-			PlotHist(ylimMin[i], ylimMax[i])
-			lines(iv, dnorm(iv, sd = sd))
-			if (plotToScreen)			
-				dev.print(pdf,file = fileName)
-			else
-				dev.off()
+				pdf(file = AddPrefix(paste0(colname, ".pdf")))
+			if (is.na(xlab[i]))
+				xlab[i] <- colname
+			if (is.na(main[i]))
+				main[i] <- sprintf("Histogram of %s", colname)
+			if (is.na(ylab[i]))
+				ylab[i] <- "Density"
+			hr <- PlotHist(ylimMin[i], ylimMax[i])
+			if (plotSelect[1]){
+				lty <- lty + 1
+				if (length(lineColor) == 1 || lineColor[1] == lineColor[2])
+					ltyl <- lty
+				else
+					ltyl <- 1
+				lines(iv, dnorm(iv, sd = sd), lty = ltyl, col = lineColor[lty])
+			}
 		}
 			
 		mean <- mean(ias)
@@ -138,34 +157,21 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 		df$PUniAsymB <- dnorm(ias, mean = mean, sd = sd)
 		UniAsymAIC[i] <- -2*sum(log(df$PUniAsymB)) + 2*2
 		UniAsymAICC[i] <- -2*sum(log(df$PUniAsymB)) + 2*AICC(2,n)	
-		mean2[i] <- mean
-		sd2[i] <- sd
-		switch(i,
-			f <- function(x){dnorm(x, mean = mean2[1], sd = sd2[1])},
-			f <- function(x){dnorm(x, mean = mean2[2], sd = sd2[2])},
-			f <- function(x){dnorm(x, mean = mean2[3], sd = sd2[3])},
-			f <- function(x){dnorm(x, mean = mean2[4], sd = sd2[4])},
-			f <- function(x){dnorm(x, mean = mean2[5], sd = sd2[5])},
-			f <- function(x){dnorm(x, mean = mean2[6], sd = sd2[6])},
-			f <- function(x){dnorm(x, mean = mean2[7], sd = sd2[7])},
-			f <- function(x){dnorm(x, mean = mean2[8], sd = sd2[8])},
-			f <- function(x){dnorm(x, mean = mean2[9], sd = sd2[9])},
-			f <- function(x){dnorm(x, mean = mean2[10], sd = sd2[10])},
-		)
+
+		f <- eval(parse(text = paste("function(x){dnorm(x, mean =", mean, 
+			", sd =", sd,")}", srcfile = NULL)))	
+
 		UnimodalAsymmetric[[i]] = list(AIC = UniAsymAIC[i], AICc = UniAsymAICC[i], 
 			mean = mean, sd = sd, f = f)
 		names(UnimodalAsymmetric)[i] = colname
-		if (plotGraph){
-			fileName = sprintf("%s-%s-Unimodal-Asymmetric.pdf", 
-				filePrefix, colname)
-			if (!plotToScreen)
-				pdf(file = fileName)
-			PlotHist(ylimMin[i], ylimMax[i])
-			lines(iv, dnorm(iv, mean = mean, sd = sd))
-			if (plotToScreen)			
-				dev.print(pdf,file = fileName)
+		if (plotGraph & plotSelect[2]){
+			lty <- lty + 1
+			if (length(lineColor) == 1 || lineColor[1] == lineColor[2])
+				ltyl <- lty
 			else
-				dev.off()
+				ltyl <- 1
+			lines(iv, dnorm(iv, mean = mean, sd = sd), lty = ltyl, 
+				col = lineColor[lty])
 		}
 		
 		DBiNorm <- function(ias, mean = 0, sd = 1, positiveRatio = 0.5) {
@@ -192,34 +198,22 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 			mean = coef(BiSymFit)["mean"]
 			sd = coef(BiSymFit)["sd"]
 		}
-		mean3[i] <- mean
-		sd3[i] <- sd
-		switch(i,
-			f <- function(x){DBiNorm(x, mean = mean3[1], sd = sd3[1])},
-			f <- function(x){DBiNorm(x, mean = mean3[2], sd = sd3[2])},
-			f <- function(x){DBiNorm(x, mean = mean3[3], sd = sd3[3])},
-			f <- function(x){DBiNorm(x, mean = mean3[4], sd = sd3[4])},
-			f <- function(x){DBiNorm(x, mean = mean3[5], sd = sd3[5])},
-			f <- function(x){DBiNorm(x, mean = mean3[6], sd = sd3[6])},
-			f <- function(x){DBiNorm(x, mean = mean3[7], sd = sd3[7])},
-			f <- function(x){DBiNorm(x, mean = mean3[8], sd = sd3[8])},
-			f <- function(x){DBiNorm(x, mean = mean3[9], sd = sd3[9])},
-			f <- function(x){DBiNorm(x, mean = mean3[10], sd = sd3[10])},
-		)
+
+		f <- eval(parse(text = paste("function(x){0.5*dnorm(x, mean =", mean,
+			", sd =", sd,") + 0.5*dnorm(x, mean =", - mean,", sd =", sd, 
+			")}", srcfile = NULL)))	
+
 		BimodalSymmetric[[i]] <- list(AIC = BiSymAIC[i], AICc = BiSymAICC[i], 
 			mean = mean, sd = sd, f = f)
 		names(BimodalSymmetric)[i] = colname
-		if (plotGraph){
-			fileName = sprintf("%s-%s-Bimodal-Symmetric.pdf", 
-				filePrefix, colname)
-			if (!plotToScreen)
-				pdf(file = fileName)
-			PlotHist(ylimMin[i], ylimMax[i])
-			lines(iv, DBiNorm(iv, mean = mean, sd = sd))
-			if (plotToScreen)			
-				dev.print(pdf,file = fileName)
+		if (plotGraph & plotSelect[3]){
+			lty <- lty + 1
+			if (length(lineColor) == 1 || lineColor[1] == lineColor[2])
+				ltyl <- lty
 			else
-				dev.off()
+				ltyl <- 1
+			lines(iv, DBiNorm(iv, mean = mean, sd = sd), lty = ltyl, 
+				col = lineColor[lty])
 		}	
 		
 		mean <- mean(abs(ias))
@@ -236,8 +230,13 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 				sdStartAsymmetric[i] <- sd
 			if (is.na(positiveRatioStartAsymmetric[i]))
 				positiveRatioStartAsymmetric[i] <- positiveRatio
-			BiAsymFunc <- function(mean = 2, sd =1 , positiveRatio = 0.5)
-					-sum(log(DBiNorm(ias, mean = mean, sd = sd, positiveRatio = positiveRatio)))
+			BiAsymFunc <- function(mean = 2, sd =1 , positiveRatio = 0.5){
+				if(positiveRatio < 0)
+					positiveRatio <- 0
+				if(positiveRatio > 1)
+					positiveRatio <- 1
+				-sum(log(DBiNorm(ias, mean = mean, sd = sd, positiveRatio = positiveRatio)))
+			}
 			#BiAsymFit <- mle(BiAsymFunc,lower=c(-100,0,0), upper=c(100,50,1)) 
 				#bounds can only be used with method L-BFGS-B or Brent
 			BiAsymFit <- mle(BiAsymFunc, start = list(mean = meanStartAsymmetric[i], 
@@ -248,60 +247,85 @@ function(df, cols = NA, fixSignApproximation = FALSE, plotGraph = TRUE,
 			sd <- coef(BiAsymFit)["sd"]
 			positiveRatio <- coef(BiAsymFit)["positiveRatio"]
 		}
-		mean4[i] <- mean
-		sd4[i] <- sd
-		positiveRatio4[i] <- positiveRatio
-		switch(i,
-			f <- function(x){DBiNorm(x, mean = mean4[1], sd = sd4[1], positiveRatio4[1])},
-			f <- function(x){DBiNorm(x, mean = mean4[2], sd = sd4[2], positiveRatio4[2])},
-			f <- function(x){DBiNorm(x, mean = mean4[3], sd = sd4[3], positiveRatio4[3])},
-			f <- function(x){DBiNorm(x, mean = mean4[4], sd = sd4[4], positiveRatio4[4])},
-			f <- function(x){DBiNorm(x, mean = mean4[5], sd = sd4[5], positiveRatio4[5])},
-			f <- function(x){DBiNorm(x, mean = mean4[6], sd = sd4[6], positiveRatio4[6])},
-			f <- function(x){DBiNorm(x, mean = mean4[7], sd = sd4[7], positiveRatio4[7])},
-			f <- function(x){DBiNorm(x, mean = mean4[8], sd = sd4[8], positiveRatio4[8])},
-			f <- function(x){DBiNorm(x, mean = mean4[9], sd = sd4[9], positiveRatio4[9])},
-			f <- function(x){DBiNorm(x, mean = mean4[10], sd = sd4[10], positiveRatio4[10])},
-		)
+
+		f <- eval(parse(text = paste("function(x){", positiveRatio, "*dnorm(x, mean =", mean,
+			", sd =", sd,") +", 1 - positiveRatio, "*dnorm(x, mean =", - mean,", sd =", sd, 
+			")}", srcfile = NULL)))	
+
 		BimodalAsymmetric[[i]] <- list(AIC = BiAsymAIC[i], AICc = BiAsymAICC[i], 
 			mean = mean, sd = sd, positiveRatio = positiveRatio, f = f)
 		names(BimodalAsymmetric)[i] = colname
 		if (plotGraph){
-			fileName = sprintf("%s-%s-Bimodal-Asymmetric.pdf", 
-				filePrefix, colname)
-			if (!plotToScreen)
-				pdf(file = fileName)
-			PlotHist(ylimMin[i], ylimMax[i])
-			lines(iv, DBiNorm(iv, mean = mean, sd = sd, positiveRatio = positiveRatio))
+			if (plotSelect[4]){
+				lty <- lty + 1
+				if (length(lineColor) == 1 || lineColor[1] == lineColor[2])
+					ltyl <- lty
+				else
+					ltyl <- 1
+				lines(iv, DBiNorm(iv, mean = mean, sd = sd, 
+						positiveRatio = positiveRatio), lty = ltyl, 
+					col = lineColor[lty])
+			}
+			if (showLegend){
+				if (length(lineColor) == 1 || lineColor[1] == lineColor[2])
+					ltyl <- 1:sum(plotSelect)
+				else
+					ltyl <- 1
+				legend("topright", lty = ltyl, legend = modelName[plotSelect], 
+					col = lineColor)
+			}
+			if (freqAxis){
+				par(new = T)
+				if (is.na(ylimMax[i]))
+					ylim = NULL
+				else
+					ylim = c(0, ylimMax[i]*length(ias)
+						*(hr$breaks[2] - hr$breaks[1]))
+				hist(ias, freq = T, breaks = breaks, 
+					ylim = ylim, xaxt = "n", yaxt = "n", 
+					xlab = "", ylab = "", main = "")
+				axis(4)
+				mtext("Number of individuals", side=4, line = 3)
+			}
 			if (plotToScreen)			
-				dev.print(pdf,file = fileName)
+				dev.print(pdf, file = AddPrefix(paste0(colname, ".pdf")))
 			else
 				dev.off()
 		}
+
+		AIC.df[i] <- c(UniSymAIC[i], UniAsymAIC[i], BiSymAIC[i], BiAsymAIC[i])
+		colnames(AIC.df)[i] <- colname
+		AICc.df[i] <- c(UniSymAICC[i], UniAsymAICC[i], BiSymAICC[i], BiAsymAICC[i])
+		colnames(AICc.df)[i] <- colname
 	}
 	
-	PrintNameValue <- function(s, x){
-		cat(sprintf("   %-20s", s), sprintf("%9.3f", x), "\n")
-	}
-	cat(sprintf("%-20s   ", "AIC:"))	
-	for (col in cols)
-		cat(sprintf("%10s", colnames(df)[col]))
-	cat("\n")		
-	PrintNameValue("Unimodal Symmetric", UniSymAIC)
-	PrintNameValue("Unimodal Asymmetric", UniAsymAIC)
-	PrintNameValue("Bimodal Symmetric", BiSymAIC)
-	PrintNameValue("Bimodal Asymmetric", BiAsymAIC)	
-	cat(sprintf("%-20s   ", "AICc:"))	
-	#for (col in cols)
-	#	cat(sprintf("%10s", colnames(df)[col]))
-	cat("\n")		
-	PrintNameValue("Unimodal Symmetric", UniSymAICC)
-	PrintNameValue("Unimodal Asymmetric", UniAsymAICC)
-	PrintNameValue("Bimodal Symmetric", BiSymAICC)
-	PrintNameValue("Bimodal Asymmetric", BiAsymAICC)
+	cat("AIC:\n")
+	print(format(AIC.df, digits = 2, nsmall = nsmall))
+	cat("\n")
+	cat("AICc:\n")
+	print(format(AICc.df, digits = 2, nsmall = nsmall))
+	cat("\n")
 	
-	return(list(UnimodalSymmetric = UnimodalSymmetric, 
+	if (fileType == "CSV"){
+		write.csv(AIC.df, file = AddPrefix("AIC.csv"))
+		write.csv(AICc.df, file = AddPrefix("AICc.csv"))
+	}
+	
+	if (fileType == "TEXT"){
+		colN <- colnames(AIC.df)[1]
+		colnames(AIC.df)[1] <- paste0("\t", colN)
+		write.table(format(AIC.df, digits = 2, nsmall = nsmall), col.names = T, 
+			file = AddPrefix("AIC.txt"), append = F, quote = F, sep = "\t")
+		colN <- colnames(AICc.df)[1]
+		colnames(AICc.df)[1] <- paste0("\t", colN)
+		write.table(format(AICc.df, digits = 2, nsmall = nsmall), col.names = T, 
+			file = AddPrefix("AICc.txt"), append = F, quote = F, sep = "\t")
+	}
+
+	result <- list(AIC = AIC.df, AICc = AICc.df, UnimodalSymmetric = UnimodalSymmetric, 
 		UnimodalAsymmetric = UnimodalAsymmetric,
 		BimodalSymmetric = BimodalSymmetric, 
-		BimodalAsymmetric = BimodalAsymmetric))
+		BimodalAsymmetric = BimodalAsymmetric)
+	names(result)[3:6] <- modelName
+	return(result)
 }
